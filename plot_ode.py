6b1d8f71e66d53c69bf07fa9ed364d9c3f43d1cf
AndresPerez07 <56890923+AndresPerez07@users.noreply.github.com>
2019-11-12 13:33:50 -0500

    Add files via upload

--- plot_ode.py
+++ plot_ode.py
@@ -1,39 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#%%
-nums = [0.1,0.01,1]
 
-fig = plt.figure(figsize=(10,10))
+
+nums = [0.0001, 0.001, 0.01, 0.1, 0.3, 0.4,0.5,0.8,0.9, 1.0]
+
+fig = plt.figure(figsize=(10,15))
 x = np.linspace(0,4,100)
-for i in range (3):
-    data_exp = np.loadtxt("euler_exp_{}.txt".format(i))
-    data_imp = np.loadtxt("euler_imp_{}.txt".format(i))
 
-    ax = fig.add_subplot(3,1,i+1)
-    ax.plot(data_exp[:,0],data_exp[:,1], label="Euler_Explicit")
-    ax.plot(data_imp[:,0],data_imp[:,1], label="Euler_Implicit")
-    ax.plot(x,np.exp(-x), label = "Analitica")
+for i in range(10):
+    data_exp = np.loadtxt("euler_exp_{}.txt".format(i))
+
+    ax = fig.add_subplot(5,2,i+1)
+    ax.plot(data_exp[:,0], data_exp[:,1], label="euler_explicit")
+    ax.plot(x,np.exp(-x), label="analitica")
     ax.set_title("$\omega\ *\ dt\ =\ {}$".format(nums[i]))
     ax.legend()
 
-    plt.savefig("Soluciones_euler.png")
-    plt.close()
-#%%
-prueba = np.arange(0.1,1.1,0.1)
+plt.savefig("soluciones_euler_exp.png")
+plt.close()
 
 fig2 = plt.figure(figsize=(10,15))
+x = np.linspace(0,4,100)
 
-for i in range (10):
-    data_exp = np.loadtxt("muere_exp_{}.txt".format(i))
-    data_imp = np.loadtxt("muere_imp_{}.txt".format(i))
+for i in range(10):
+    data_imp = np.loadtxt("euler_imp_{}.txt".format(i))
 
     ax = fig2.add_subplot(5,2,i+1)
-    ax.plot(data_exp[:,0],data_exp[:,1], label="Euler_Explicit")
-    ax.plot(data_imp[:,0],data_imp[:,1], label="Euler_Implicit")
-    ax.plot(x,np.exp(-x), label = "Analitica")
-    ax.set_title("$\omega\ *\ dt\ =\ {}$".format(prueba[i]))
+    ax.plot(data_imp[:,0], data_imp[:,1], label="euler_implicit")
+    ax.plot(x,np.exp(-x), label="analitica")
+    ax.set_title("$\omega\ *\ dt\ =\ {}$".format(nums[i]))
     ax.legend()
 
-    plt.savefig("Exp_muere_euler.png")
-    plt.close()
-#%%
+plt.savefig("soluciones_euler_imp.png")
+plt.close()
